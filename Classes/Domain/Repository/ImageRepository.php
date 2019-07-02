67fce28eba88b9028a578b88b835f4f8a34d76a3
Gaétan Basile <gaetan.js.basile@outlook.fr>
2019-07-02 11:38:35 +0200

    Add constructor dependency injection

---
+++ Classes/Domain/Repository/ImageRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Gtasjjat\GalerieGbtlabsgjvjfavtr\Domain\Repository;
+
+/***
+ *
+ * This file is part of the "galerie_cantonnais" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ *  (c) 2019 Gaétan BASILE <gaetan.basile.1@etu.u-bordeaux>
+ *           Théo LECOQ
+ *           Aurore BAUME
+ *           Sebastien GAYA
+ *           Julien VANELIAN
+ *           Jessica FERREIRA
+ *           Alexandre VIAS
+ *           Thibault ROBERT
+ *
+ ***/
+
+/**
+ * The repository for Images
+ */
+class ImageRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
+{
+
+    public function findLatest()
+    {
+        return $this->createQuery()
+            ->setOrderings([
+                'publicationDate' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_DESCENDING
+            ])
+            ->setLimit(5)
+            ->execute();
+    }
+}
